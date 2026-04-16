937a1d88c83ac8ef22b1b0570eeb82a62ef34f5d
chimelu <ezeoguinechimelu@gmail.com>
2026-04-16 13:15:15 +0100

    g

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import AdminLayout from './layouts/AdminLayout';
-// import UsersPage from './pages/UsersPage';
-// import TransactionsPage from './pages/TransactionsPage';
-// import WithdrawalsPage from './pages/WithdrawalsPage';
+import UsersPage from './pages/UsersPage';
+import TransactionsPage from './pages/TransactionsPage';
+import WithdrawalsPage from './pages/WithdrawalsPage';
 import SupportPage from './pages/SupportPage';
 import DeleteAccountPage from './pages/DeleteAccountPage';
 import './App.css';
@@ -12,9 +12,9 @@ export default function App() {
     <Routes>
       <Route element={<AdminLayout />}>
         <Route path="/" element={<Navigate to="/support" replace />} />
-        {/* <Route path="/users" element={<UsersPage />} /> */}
-        {/* <Route path="/transactions" element={<TransactionsPage />} /> */}
-        {/* <Route path="/withdrawals" element={<WithdrawalsPage />} /> */}
+        <Route path="/users" element={<UsersPage />} />
+        <Route path="/transactions" element={<TransactionsPage />} />
+        <Route path="/withdrawals" element={<WithdrawalsPage />} />
       </Route>
       <Route path="/support" element={<SupportPage />} />
       <Route path="/delete-account" element={<DeleteAccountPage />} />
